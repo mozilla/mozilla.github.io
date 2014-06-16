548f9fd0d8b8307bbe7059a47b3bb662b5918a9d
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2014-06-17 01:43:56 +0200

    Recent amendments.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,48 +1,82 @@
 /*! Lazy Load 1.9.3 - MIT license - Copyright 2010-2013 Mika Tuupola */
 !function(a,b,c,d){var e=a(b);a.fn.lazyload=function(f){function g(){var b=0;i.each(function(){var c=a(this);if(!j.skip_invisible||c.is(":visible"))if(a.abovethetop(this,j)||a.leftofbegin(this,j));else if(a.belowthefold(this,j)||a.rightoffold(this,j)){if(++b>j.failure_limit)return!1}else c.trigger("appear"),b=0})}var h,i=this,j={threshold:0,failure_limit:0,event:"scroll",effect:"show",container:b,data_attribute:"original",skip_invisible:!0,appear:null,load:null,placeholder:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"};return f&&(d!==f.failurelimit&&(f.failure_limit=f.failurelimit,delete f.failurelimit),d!==f.effectspeed&&(f.effect_speed=f.effectspeed,delete f.effectspeed),a.extend(j,f)),h=j.container===d||j.container===b?e:a(j.container),0===j.event.indexOf("scroll")&&h.bind(j.event,function(){return g()}),this.each(function(){var b=this,c=a(b);b.loaded=!1,(c.attr("src")===d||c.attr("src")===!1)&&c.is("img")&&c.attr("src",j.placeholder),c.one("appear",function(){if(!this.loaded){if(j.appear){var d=i.length;j.appear.call(b,d,j)}a("<img />").bind("load",function(){var d=c.attr("data-"+j.data_attribute);c.hide(),c.is("img")?c.attr("src",d):c.css("background-image","url('"+d+"')"),c[j.effect](j.effect_speed),b.loaded=!0;var e=a.grep(i,function(a){return!a.loaded});if(i=a(e),j.load){var f=i.length;j.load.call(b,f,j)}}).attr("src",c.attr("data-"+j.data_attribute))}}),0!==j.event.indexOf("scroll")&&c.bind(j.event,function(){b.loaded||c.trigger("appear")})}),e.bind("resize",function(){g()}),/(?:iphone|ipod|ipad).*os 5/gi.test(navigator.appVersion)&&e.bind("pageshow",function(b){b.originalEvent&&b.originalEvent.persisted&&i.each(function(){a(this).trigger("appear")})}),a(c).ready(function(){g()}),this},a.belowthefold=function(c,f){var g;return g=f.container===d||f.container===b?(b.innerHeight?b.innerHeight:e.height())+e.scrollTop():a(f.container).offset().top+a(f.container).height(),g<=a(c).offset().top-f.threshold},a.rightoffold=function(c,f){var g;return g=f.container===d||f.container===b?e.width()+e.scrollLeft():a(f.container).offset().left+a(f.container).width(),g<=a(c).offset().left-f.threshold},a.abovethetop=function(c,f){var g;return g=f.container===d||f.container===b?e.scrollTop():a(f.container).offset().top,g>=a(c).offset().top+f.threshold+a(c).height()},a.leftofbegin=function(c,f){var g;return g=f.container===d||f.container===b?e.scrollLeft():a(f.container).offset().left,g>=a(c).offset().left+f.threshold+a(c).width()},a.inviewport=function(b,c){return!(a.rightoffold(b,c)||a.leftofbegin(b,c)||a.belowthefold(b,c)||a.abovethetop(b,c))},a.extend(a.expr[":"],{"below-the-fold":function(b){return a.belowthefold(b,{threshold:0})},"above-the-top":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-screen":function(b){return a.rightoffold(b,{threshold:0})},"left-of-screen":function(b){return!a.rightoffold(b,{threshold:0})},"in-viewport":function(b){return a.inviewport(b,{threshold:0})},"above-the-fold":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-fold":function(b){return a.rightoffold(b,{threshold:0})},"left-of-fold":function(b){return!a.rightoffold(b,{threshold:0})}})}(jQuery,window,document);
 
-$( document ).ready(function() {
+/* Hash */
+function hash(str, asString, seed) {
+    if (str == null) {
+        str = 'null';
+    }
+
+    var i, l,
+        hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+    for (i = 0, l = str.length; i < l; i++) {
+        hval ^= str.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+
+    if (asString) {
+        return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+    }
+
+    return hval >>> 0;
+}
+
+/* Main */
+$(document).ready(function () {
+    // Variebles
+    var members = [],
+        projects = [];
+
     // Members
-    for (var i = 1, stop = false; i < 5; i++) {
+    for (var i = 1; i < 10; i++) {
         $.ajax({
             url: "https://api.github.com/orgs/mozilla/members",
             data: {
                 page: i,
                 per_page: 100
             },
-            success: function( data ) {
-                if( data.length == 0 ){ stop = true; }
+            success: function (data) {
+                if (data.length == 0) {
+                    for (var j = 0, html = ''; j < members.length; j++) {
+                        html = '<a href="' + members[j].html_url + '" target="_blank"><img data-original="' + members[j].avatar_url + 's=60" alt="' + members[j].login + '" /></a>'
+                        $('.members').append(html);
 
-                for (var j = 0, html = ''; j < data.length; j++) {
-                    html = '<a href="' + data[j].html_url + '" target="_blank"><img data-original="' + data[j].avatar_url + 's=60" alt="' + data[j].login + '" /></a>'
-                    $('.members').append(html);
-                };
-
-                $("img").lazyload();
+                        $("img").lazyload();
+                    }
+                } else {
+                    $.merge(members, data);
+                }
             }
         });
-
-        if( stop == true ){ break; }
     }
 
     // Projects
-    for (var i = 1, stop = false; i < 10; i++) {
+    for (var i = 1; i < 10; i++) {
         $.ajax({
             url: "https://api.github.com/orgs/mozilla/repos",
             data: {
                 page: i,
                 per_page: 100
             },
-            success: function( data ) {
-                if( data.length == 0 ){ stop = true; }
+            success: function (data) {
+                if (data.length == 0) {
+                    projects.sort(function (obj1, obj2) {
+                        return obj2.stargazers_count - obj1.stargazers_count;
+                    });
 
-                for (var j = 0, html = ''; j < data.length; j++) {
-                    html = '<a href="' + data[j].html_url + '" target="_blank">' + data[j].full_name + ' <span>' + data[j].stargazers_count + '</span></a>'
-                    $('.projects').append(html);
-                };
+                    for (var j = 0, html = ''; j < projects.length; j++) {
+                        if (document.getElementById(hash(projects[j].language, 10))) {} else {
+                            $(".projects").append('<p><kbd>[' + projects[j].language + ']</kbd></p><div id="' + hash(projects[j].language, 10) + '"></div>');
+                        }
+
+                        html = '<a href="' + projects[j].html_url + '" target="_blank">' + projects[j].full_name + ' <span>' + projects[j].stargazers_count + '</span></a>'
+                        $('.projects #' + hash(projects[j].language, 10)).append(html);
+                    }
+                } else {
+                    $.merge(projects, data);
+                }
             }
         });
-
-        if( stop == true ){ break; }
     }
 });
